fc3ed69fb56ab4264e9373ea2aa6dc5b69fb0d9a
chaikivskti.t <chaikivskyi.t@gmail.com>
2025-09-04 12:11:51 +0200

    add axios library

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,23 @@
-export const fetchPhotosByQuery = searchedQuery => {
-  const requestParams = new URLSearchParams({
+import axios from 'axios';
+
+export const fetchPhotosByQuery = async searchedQuery => {
+  const requestParams = {
     key: import.meta.env.VITE_PIXABAY_API,
     image_type: 'photo',
     per_page: 40,
     safesearch: true,
     orientation: 'horizontal',
     q: searchedQuery,
-  });
+  };
 
-  return fetch(`https://pixabay.com/api/?${requestParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  });
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: requestParams,
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      `HTTP error! ${error.response?.status || ''} ${error.message}`
+    );
+  }
 };
